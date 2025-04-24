db3f0aad20de54b96d212fb9278d9a63538bcadf
Cristian Ramonda <cris.ram94@gmail.com>
2025-04-24 00:15:58 -0300

    Cartwidget, se quita boton y se hace link, se agrega hover, numero oculto cuando es 0

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,46 +1,24 @@
 import { useState, useContext, useEffect } from "react"
-import { Button } from "react-bootstrap"
 import { Link } from "react-router"
 import { ShoppingCart } from "lucide-react"
 import { CartContext } from "../context/CartContext"
 
 function CartWidget() {
     const [cartElements, setCartElem] = useState(0)
-    const { cartItems, getQuantity } = useContext(CartContext)
+    const { getQuantity } = useContext(CartContext)
 
     useEffect(() => {
         setCartElem(getQuantity)
     }, [getQuantity])
 
-    if (cartElements != 0) {
-        return (
-            <>
-                <div style={{
-                    color: 'white',
-                    display: 'flex'
-                }}>
-                    <ShoppingCart style={{ marginRight: '5px' }} />{cartElements}
-                </div>
-                <Button as={Link} to='/cart/' className="mx-4" variant="light">
-                    Ir al carro
-                </Button>
-            </>
-        )
-    } else {
-        return (
-            <>
-                <div style={{
-                    color: 'white',
-                    display: 'flex'
-                }}>
-                    <ShoppingCart style={{ marginRight: '5px' }} />{cartElements}
-                </div>
-                <Button className="mx-4" style={{ visibility: 'hidden' }}>
-                    Ir al carro
-                </Button>
-            </>
-        )
-    }
+    return (
+            <Link className="cart-logo" to='/cart/'>
+                    <ShoppingCart style={{ marginRight: '5px' }} />
+                    {cartElements > 0 ?
+                        <div>{cartElements}</div> :
+                        <div style={{ visibility: 'hidden' }}>{cartElements}</div>}
+            </Link>
+    )
 }
 
 export default CartWidget
